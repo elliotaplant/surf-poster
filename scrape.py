f5110f847da26eedce671ae9fe5e752eaa509d9a
Elliot Plant <elliotaplant@gmail.com>
2020-09-15 21:08:19 -0700

    Use regex instead of lxml for scraping

--- scrape.py
+++ scrape.py
@@ -1,38 +1,28 @@
 # Fetch surf conditions from surfline's static site
-from lxml import html
 import requests
+import re
 
 
 class ConditionFetcher:
     def __init__(self):
-        self.surf_height_selector = '.quiver-spot-forecast-summary__stat-container--surf-height .quiver-surf-height'
-        self.water_temp_selector = '.quiver-wetsuit-recommender .quiver-water-temp div'
-        self.wind_speed_selector = '.quiver-spot-forecast-summary__stat-container--wind .quiver-reading'
+        self.surf_height_selector = 'Surf height</div><span class="quiver-surf-height">(.+?)<sup>FT'
+        self.water_temp_selector = 'Tourmaline Water Temp"/>(.+?)<'
+        self.wind_speed_selector = (
+            '.Wind</div><div class="quiver-conditions-stats__stat-reading"><span class="quiver-reading">(.+?)<sup>'
+        )
 
     def get_conditions(self):
         page = requests.get('https://www.surfline.com/surf-report/old-man-s-at-tourmaline/5842041f4e65fad6a77088c4')
-        tree = html.fromstring(page.content)
+        text = page.text
 
-        surf_height = tree.cssselect(self.surf_height_selector)[0].text_content()
-        water_temp = tree.cssselect(self.water_temp_selector)[0].text_content()
-        wind_speed = tree.cssselect(self.wind_speed_selector)[0].text_content()
+        surf_height = re.search(self.surf_height_selector, text).group(1)
+        water_temp = re.search(self.water_temp_selector, text).group(1)
+        wind_speed = re.search(self.wind_speed_selector, text).group(1)
 
-        return self.parse_surf_height(surf_height), self.parse_water_temp(water_temp), self.parse_wind_speed(wind_speed)
+        return self.parse_surf_height(surf_height), int(water_temp), int(wind_speed)
 
-    # Expected format is %d-%dFT, eg 1-2FT
+    # Expected format is %d-%d, eg 1-2
     def parse_surf_height(self, raw_surf_height):
-        without_suffix = raw_surf_height[:-2]
-        low, high = without_suffix.split('-')
+        low, high = raw_surf_height.split('-')
         avg = (int(high) + int(low)) / 2
         return avg
-
-    # Expected format is %d%d - %d%d ºF, eg 62 - 65 ºF
-    def parse_water_temp(self, raw_water_temp):
-        without_suffix = raw_water_temp[:-3]
-        low, high = without_suffix.split(' - ')
-        avg = (int(high) + int(low)) / 2
-        return avg
-
-    # Expected format is %d+KTS
-    def parse_wind_speed(self, raw_wind_speed):
-        return int(raw_wind_speed[:-3])
