ec1fe71ba3efe17470c34f68a6449ad58ba3403a
Elliot Plant <elliotaplant@gmail.com>
2020-09-22 12:52:20 -0700

    Change wind speed to surf quality

--- actuate.py
+++ actuate.py
@@ -4,14 +4,14 @@ import time
 
 SURF_HEIGHT_PIN_NUMBER = 20
 WATER_TEMP_PIN_NUMBER = 16
-WIND_SPEED_PIN_NUMBER = 21
+SURF_QUALITY_PIN_NUMBER = 21
 
 SERVO_PWM_FREQUENCY = 50  # Hz
 DUTY_CYCLE_RANGE = [12.5, 2.5]
 
 SURF_HEIGHT_RANGE = [0, 6]
 WATER_TEMP_RANGE = [55, 75]
-WIND_SPEED_RANGE = [0, 15]
+SURF_QUALITY_RANGE = [1, 10]
 
 
 class ServoController:
@@ -20,34 +20,34 @@ class ServoController:
 
         GPIO.setup(SURF_HEIGHT_PIN_NUMBER, GPIO.OUT)
         GPIO.setup(WATER_TEMP_PIN_NUMBER, GPIO.OUT)
-        GPIO.setup(WIND_SPEED_PIN_NUMBER, GPIO.OUT)
+        GPIO.setup(SURF_QUALITY_PIN_NUMBER, GPIO.OUT)
 
         self.surf_height_pin = GPIO.PWM(SURF_HEIGHT_PIN_NUMBER, SERVO_PWM_FREQUENCY)
         self.surf_height_pin.start(3)
         self.water_temp_pin = GPIO.PWM(WATER_TEMP_PIN_NUMBER, SERVO_PWM_FREQUENCY)
         self.water_temp_pin.start(3)
-        self.wind_speed_pin = GPIO.PWM(WIND_SPEED_PIN_NUMBER, SERVO_PWM_FREQUENCY)
-        self.wind_speed_pin.start(3)
+        self.surf_quality_pin = GPIO.PWM(SURF_QUALITY_PIN_NUMBER, SERVO_PWM_FREQUENCY)
+        self.surf_quality_pin.start(3)
 
     def stop(self):
         logging.info("Cleaning up")
         self.surf_height_pin.stop()
         self.water_temp_pin.stop()
-        self.wind_speed_pin.stop()
+        self.surf_quality_pin.stop()
         GPIO.cleanup()
 
-    def set_to_angles(self, surf_height, water_temp, wind_speed):
+    def set_to_angles(self, surf_height, water_temp, surf_quality):
         surf_height_duty = self._convert_to_duty_cycle(surf_height, SURF_HEIGHT_RANGE)
         water_temp_duty = self._convert_to_duty_cycle(water_temp, WATER_TEMP_RANGE)
-        wind_speed_duty = self._convert_to_duty_cycle(wind_speed, WIND_SPEED_RANGE)
-        logging.info(f"Surf height duty cycle: {surf_height_duty}, Water temp duty: {water_temp_duty}, Wind speed duty: {wind_speed_duty}")
+        surf_quality_duty = self._convert_to_duty_cycle(surf_quality, SURF_QUALITY_RANGE)
+        logging.info(f"Surf height duty cycle: {surf_height_duty}, Water temp duty: {water_temp_duty}, Wind speed duty: {surf_quality_duty}")
         self.surf_height_pin.ChangeDutyCycle(surf_height_duty)
         self.water_temp_pin.ChangeDutyCycle(water_temp_duty)
-        self.wind_speed_pin.ChangeDutyCycle(wind_speed_duty)
+        self.surf_quality_pin.ChangeDutyCycle(surf_quality_duty)
         time.sleep(1)
         self.surf_height_pin.ChangeDutyCycle(0)
         self.water_temp_pin.ChangeDutyCycle(0)
-        self.wind_speed_pin.ChangeDutyCycle(0)
+        self.surf_quality_pin.ChangeDutyCycle(0)
         time.sleep(1)
 
     def _convert_to_duty_cycle(self, metric, metric_range):
--- scrape.py
+++ scrape.py
@@ -7,9 +7,7 @@ class ConditionFetcher:
     def __init__(self):
         self.surf_height_selector = 'Surf height</div><span class="quiver-surf-height">(.+?)<sup>FT'
         self.water_temp_selector = 'Tourmaline Water Temp"/>(.+?)<'
-        self.wind_speed_selector = (
-            '.Wind</div><div class="quiver-conditions-stats__stat-reading"><span class="quiver-reading">(.+?)<sup>'
-        )
+        self.surf_quality_selector = 'quiver-colored-condition-bar quiver-colored-condition-bar--.+?\">(.+?)</div>'
 
     def get_conditions(self):
         page = requests.get('https://www.surfline.com/surf-report/old-man-s-at-tourmaline/5842041f4e65fad6a77088c4')
@@ -17,12 +15,27 @@ class ConditionFetcher:
 
         surf_height = re.search(self.surf_height_selector, text).group(1)
         water_temp = re.search(self.water_temp_selector, text).group(1)
-        wind_speed = re.search(self.wind_speed_selector, text).group(1)
+        surf_quality = re.search(self.surf_quality_selector, text).group(1)
 
-        return self.parse_surf_height(surf_height), int(water_temp), int(wind_speed)
+        return self.parse_surf_height(surf_height), int(water_temp), self.translate_surf_quality(surf_quality)
 
     # Expected format is %d-%d, eg 1-2
     def parse_surf_height(self, raw_surf_height):
         low, high = raw_surf_height.split('-')
         avg = (int(high) + int(low)) / 2
         return avg
+    
+    def translate_surf_quality(self, surf_quality):
+        return [
+            None,
+            'FLAT',
+            'VERY POOR',
+            'POOR',
+            'POOR to FAIR',
+            'FAIR',
+            'FAIR to GOOD',
+            'GOOD',
+            'VERY GOOD',
+            'GOOD to EPIC',
+            'EPIC'
+        ].index(surf_quality)
