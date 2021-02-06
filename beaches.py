c839b0c87a1cd8e76cb79249c6efa56ef76c04e4
Elliot Plant <elliotaplant@gmail.com>
2021-02-06 14:39:50 -0800

    Fix config file formatting

--- beaches.py
+++ beaches.py
@@ -4,17 +4,17 @@ beaches = {
     'tourmaline': {
         'surfline_id': 'old-man-s-at-tourmaline/5842041f4e65fad6a77088c4',
         'dial_range': {
-            'surf_height' = [0, 5],
-            'water_temp' = [55, 80],
-            'surf_quality' = [1, 10],
+            'surf_height': [0, 5],
+            'water_temp': [55, 80],
+            'surf_quality': [1, 10],
         }
     },
     'blacks': {
         'surfline_id': 'blacks/5842041f4e65fad6a770883b',
         'dial_range': {
-            'surf_height' = [0, 10],
-            'water_temp' = [55, 80],
-            'surf_quality' = [1, 10],
+            'surf_height': [0, 10],
+            'water_temp': [55, 80],
+            'surf_quality': [1, 10],
         }
     },
 }
